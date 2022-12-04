ecba7dfb4e0802c384fd40b375ba7228fbd7af85
somesh <someshkumarbhaskar@gmail.com>
2022-12-04 21:29:22 +0530

    material texture added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
-import * as THREE from "three";
-import { BoxGeometry, CylinderGeometry, FloatType, MeshBasicMaterial, MeshStandardMaterial, SphereGeometry, TextureLoader, Vector2 } from "three";
-import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils';
+import { ACESFilmicToneMapping, BoxGeometry, BufferGeometry, Color, CylinderGeometry, FloatType, Mesh, MeshPhysicalMaterial, MeshStandardMaterial, PerspectiveCamera, PMREMGenerator, Scene, sRGBEncoding, Texture, TextureLoader, Vector2, WebGLRenderer } from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils';
 
 @Component({
   selector: 'app-root',
@@ -14,39 +13,52 @@ export class AppComponent implements AfterViewInit {
 
   @ViewChild('canvas') canvasRef: ElementRef;
 
-  private camera!: THREE.PerspectiveCamera;
+  private camera!: PerspectiveCamera;
 
-  private renderer!: THREE.WebGLRenderer;
+  private renderer!: WebGLRenderer;
 
-  private scene: THREE.Scene = new THREE.Scene();
-  mousePos = new THREE.Vector2(0, 0);
-  pmrem !: THREE.PMREMGenerator;
-  hexagonGeometeries !: THREE.BoxGeometry | THREE.BufferGeometry;
+  private scene: Scene = new Scene();
+  mousePos = new Vector2(0, 0);
+  pmrem !: PMREMGenerator;
+  hexagonGeometeries !: BoxGeometry | BufferGeometry;
+
+  stoneGeo !: BoxGeometry | BufferGeometry;
+  dirtGeo !: BoxGeometry | BufferGeometry;
+  dirt2Geo !: BoxGeometry | BufferGeometry;
+  sandGeo !: BoxGeometry | BufferGeometry;
+  grassGeo !: BoxGeometry | BufferGeometry;
 
   async initialize() {
-    this.scene.background = new THREE.Color("white");
-    this.camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 10, 1000);
+
+    this.stoneGeo = new BoxGeometry(0, 0, 0);
+    this.dirtGeo = new BoxGeometry(0, 0, 0);
+    this.dirt2Geo = new BoxGeometry(0, 0, 0);
+    this.sandGeo = new BoxGeometry(0, 0, 0);
+    this.grassGeo = new BoxGeometry(0, 0, 0);
+
+
+    this.scene.background = new Color("white");
+    this.camera = new PerspectiveCamera(45, innerWidth / innerHeight, 10, 1000);
 
     this.camera.position.set(0, 60, 16);
     const aspectRatio = this.getAspectRatio();
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvasRef.nativeElement });
+    this.renderer = new WebGLRenderer({ antialias: true, canvas: this.canvasRef.nativeElement });
     this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.toneMapping = ACESFilmicToneMapping;
+    this.renderer.outputEncoding = sRGBEncoding;
     this.renderer.setPixelRatio(devicePixelRatio);
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
     controls.target.set(0, 0, 0);
     controls.dampingFactor = 0.5;
     controls.enableDamping = true;
 
-    this.pmrem = new THREE.PMREMGenerator(this.renderer);
+    this.pmrem = new PMREMGenerator(this.renderer);
     // this.pmrem.compileEquirectangularShader();
     this.hexagonGeometeries = new BoxGeometry(0, 0, 0);
-
     const envMapTexture = await new RGBELoader().setDataType(FloatType).loadAsync("assets/envmap.hdr");
     const envMap = this.pmrem.fromEquirectangular(envMapTexture).texture;
-
-    this.scene.add(this.createHexMap(envMap));
+    const texture = await this.loadTexture();
+    this.createHexMap(texture, envMap)
 
     this.renderer.setAnimationLoop(() => {
       controls.update();
@@ -59,35 +71,39 @@ export class AppComponent implements AfterViewInit {
   }
 
   hexGeometry(height, position) {
-    let geo = new THREE.CylinderGeometry(1, 1, height, 6, 1, false);
+    let geo = new CylinderGeometry(1, 1, height, 6, 1, false);
     geo.translate(position.x, height * 0.5, position.y);
     return geo;
   }
 
   makeHex(height, position) {
     let geo = this.hexGeometry(height, position);
-    this.hexagonGeometeries = mergeBufferGeometries([this.hexagonGeometeries, geo])
+    // this.grassGeo = mergeBufferGeometries([this.grassGeo, geo]);
+
+    if (position.x == 0) {
+      this.stoneGeo = mergeBufferGeometries([geo, this.stoneGeo]);
+    } else {
+      this.dirtGeo = mergeBufferGeometries([geo, this.dirtGeo]);
+    }
   }
 
   ngAfterViewInit(): void {
     this.initialize();
   }
+
   getAspectRatio() {
     return this.canvasRef.nativeElement.clientWidth / this.canvasRef.nativeElement.clientHeight;
   }
 
-  createHexMap(envMap) {
+  createHexMap(texture, envMap) {
     for (let i = -15; i < 15; i++) {
       for (let j = -15; j < 15; j++) {
         this.makeHex(1, this.tileToPosition(i, j));
       }
     }
-    const hexagonMesh = new THREE.Mesh(this.hexagonGeometeries, new MeshStandardMaterial({
-      envMap: envMap,
-      flatShading: true
-    }))
 
-    return hexagonMesh;
+    this.createMeshTexture(texture, envMap);
+
   }
 
   async loadTexture() {
@@ -103,24 +119,27 @@ export class AppComponent implements AfterViewInit {
     return textures;
   }
 
-
-  createMeshTexture(textures) {
-    const stoneGeo = new BoxGeometry(0, 0, 0);
-    const dirtGeo = new BoxGeometry(0, 0, 0);
-    const dirt2Geo = new BoxGeometry(0, 0, 0);
-    const sandGeo = new BoxGeometry(0, 0, 0);
-    const grassGeo = new BoxGeometry(0, 0, 0);
-      const stoneMesh= this.hexMesh(stoneGeo, textures.stone)
-      const grassMesh= this.hexMesh(grassGeo, textures.grass)
-      const dirt2Mesh= this.hexMesh(dirt2Geo, textures.dirt2)
-      const dirtMesh= this.hexMesh(dirtGeo, textures.dirt)
-      const sandMesh= this.hexMesh(sandGeo, textures.sand)
-      
-
+  createMeshTexture(textures, envMap) {
+    debugger
+    const stoneMesh = this.hexMesh(this.stoneGeo as BoxGeometry, textures.stone, envMap);
+    const grassMesh = this.hexMesh(this.grassGeo as BoxGeometry, textures.grass, envMap);
+    const dirt2Mesh = this.hexMesh(this.dirt2Geo as BoxGeometry, textures.dirt2, envMap);
+    const dirtMesh = this.hexMesh(this.dirtGeo as BoxGeometry, textures.dirt, envMap);
+    const sandMesh = this.hexMesh(this.sandGeo as BoxGeometry, textures.sand, envMap);
+    this.scene.add(stoneMesh, grassMesh, dirt2Mesh, dirtMesh, sandMesh);
   }
-  hexMesh(stoneGeo: THREE.BoxGeometry, stone: any) {
 
+  hexMesh(geo: BoxGeometry, map: Texture, envMap) {
+    let mat = new MeshPhysicalMaterial({
+      envMap: envMap,
+      envMapIntensity: 0.135,
+      flatShading: true,
+      map: map
+    });
+
+    let mesh = new Mesh(geo, mat);
+    return mesh;
   }
-  
+
 }
 
