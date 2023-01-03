c093c5fb7df0ce4a2d21ed03021bd13c385ed029
somesh <someshkumarbhaskar@gmail.com>
2023-01-03 23:05:40 +0530

    added distict

--- src/app/component/distict/distict.component.ts
+++ src/app/component/distict/distict.component.ts
@@ -1,94 +1,53 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HexGeneratorService } from 'src/app/services/hex-generator.service';
-import { australiaTileData, distictTileData } from 'src/app/tile-data';
-import { PerspectiveCamera, WebGLRenderer, Scene, PMREMGenerator, Raycaster, Vector2, Color, ACESFilmicToneMapping, PCFShadowMap, sRGBEncoding } from 'three';
+
 @Component({
   selector: 'app-distict',
   templateUrl: './distict.component.html',
   styleUrls: ['./distict.component.css']
 })
-export class DistictComponent implements OnInit, AfterViewInit {
+export class DistictComponent implements OnInit {
   tileID = null;
+  title = 'World';
+  noOfRow = 10;
+  noOfCol = 10
+
+  distict = [2, 3, 2]
+
+  gridArray = [4, 5, 6, 7, 6, 5, 4]
+  largestRow = 7;
+  currentDistict:  { x: -1, y: -1 }
+  armies = [1,2,3,7,8,7,7,7,7,7,7];
+
+  color: tileColor = {
+  }
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { tileID: string }, private hexGenerator: HexGeneratorService, public dialog: MatDialog) {
     this.tileID = data.tileID;
-  }
-
-  @ViewChild('canvas') canvasRef: ElementRef;
-
-  private camera!: PerspectiveCamera;
-  private renderer!: WebGLRenderer;
-  private scene: Scene = new Scene();
-  pmrem !: PMREMGenerator;
-  raycaster = new Raycaster();
-  mouse = new Vector2();
-
-  ngOnInit(): void {
+    console.log(this.tileID)
   }
 
 
-  async initializeScene() {
-    this.scene.background = new Color("#0E5378");
-    this.camera = this.hexGenerator.updateCamera(80, 0, 15, 0);
+ngOnInit(): void {
+}
 
-    const height = innerHeight/1.6;
-    const width = innerWidth/3.5;
+getColor(i: number) {
+  return this.color[i] ? this.color[i] : 'blue';
+}
 
+onClick(i: number, j: number) {
 
-    this.renderer = new WebGLRenderer({ antialias: true, canvas: this.canvasRef.nativeElement, alpha: true });
-    this.renderer.setSize(width, height);
-    this.renderer.toneMapping = ACESFilmicToneMapping;
-    this.renderer.outputEncoding = sRGBEncoding;
-    this.renderer.physicallyCorrectLights = true;
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = PCFShadowMap;
-    this.renderer.setPixelRatio(width/height);
-    this.pmrem = new PMREMGenerator(this.renderer);
-    this.pmrem.compileEquirectangularShader();
-    this.camera.aspect =width/height;
-    this.camera.updateProjectionMatrix();
-    const texture = await this.hexGenerator.loadTexture();
-    const envMap = await this.hexGenerator.updateEnvMap(this.pmrem);
+  // this.dialog.closeAll();
+  // this.dialog.open(BaseComponent, { data: { x: i, y: j } })
+  // this.router.navigate(['base', i, j]);
+}
+getStartingMargin(value: number, midRowValue: number) {
+  return ((midRowValue - value) * 33);
+}
 
-    const tileData = {
-      distict: distictTileData
-    };
+}
 
-    this.hexGenerator.createDistictHexMap(texture, envMap, 6, 6, this.scene, tileData);
-    const controls = this.hexGenerator.addOrbitalControl(this.camera, this.renderer);
-
-    controls.enableZoom = false;
-
-    // to disable rotation
-    controls.enableRotate = false;
-
-    // to disable pan
-    controls.enablePan = false;
-
-    this.hexGenerator.addLight(this.scene);
-
-    this.renderer.setAnimationLoop(() => {
-      controls.update();
-      this.renderer.render(this.scene, this.camera);
-    });
-
-    window.addEventListener('click', this.onClick.bind(this), false);
-  }
-
-  onClick(event) {
-
-    this.mouse.x = (event.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
-    this.mouse.y = - (event.clientY / this.renderer.domElement.clientHeight) * 2 + 1;
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-    const intersects = this.raycaster.intersectObjects(this.scene.children);
-    if (intersects.length >= 1) {
-      // do something
-      console.log(intersects[0].object["geometry"].name)
-    }
-  }
-
-  ngAfterViewInit(): void {
-    this.initializeScene();
-  }
+interface tileColor {
+  [key: number]: string
 }
